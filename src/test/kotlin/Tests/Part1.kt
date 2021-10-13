d7c36c6b23b01fd097b7778e466ee326516c1701
Yaroslav <rqpr@mail.ru>
2021-10-13 21:34:55 +0300

    Refactored

--- src/test/kotlin/Tests/Part1.kt
+++ src/test/kotlin/Tests/Part1.kt
@@ -7,6 +7,7 @@ class Part1 {
 
     @Test
     fun task1() {
+        println("Числа отразка от 1 до 99 кратные 3:")
         for (i in 1..99) {
             if (i % 3 == 0) println(i)
         }
@@ -16,7 +17,7 @@ class Part1 {
     @Test
     fun task2() {
         val k = (1..10).random()
-        println("Значение k равно $k")
+        println("Значение k равно $k \nЧисла кратные числу k:")
         for (i in 10..99) {
             if (i % k == 0) println(i)
         }
@@ -33,7 +34,7 @@ class Part1 {
         var out = res
         var len = 0
 
-        while (out !== 0) {
+        while (out != 0) {
             len++
             out /= 10
         }
@@ -51,23 +52,11 @@ class Part1 {
         var max: Int
         var min: Int
 
-        if (a > b) {
-            max = a; min = b
-        } else {
-            max = b; min = a
-        }
+        if (a > b) {max = a; min = b} else {max = b; min = a}
 
-        if (c > max) {
-            max = c
-        } else if (min > c) {
-            min = c
-        }
+        if (c > max) {max = c} else if (min > c) {min = c}
 
-        if (d > max) {
-            max = d
-        } else if (min > d) {
-            min = d
-        }
+        if (d > max) {max = d} else if (min > d) {min = d}
 
         println("Даны 4 числа: $a, $b, $c, $d.\nМаксимальное число $max.\nМинимальное число $min.")
     }
@@ -77,26 +66,26 @@ class Part1 {
     fun task5() {
         var a = 5
         var b = 10
-        var c = a
+        val c = a
 
         println("Вариант А: \nНачальные значения $a, $b")
         a = b
         b = c
-        println("Конечные результат $a, $b")
+        println("Конечный результат $a, $b")
 
-        println("Вариант Б: \nНачальные результат $a, $b")
+        println("Вариант Б: \nНачальные значения $a, $b")
         a = a + b
         b = a - b
         a = a - b
-        println("Конечные результат $a, $b")
+        println("Конечный результат $a, $b")
     }
 
 
     @Test
     fun Task6() {
-        var k = (1..27).random()
+        val k = (1..27).random()
 
-        println("Значение k равно $k \nРезультат:")
+        println("Значение k равно $k \nCумма цифр которые равны числу k :")
 
         for (i in 100..999) {
             var n = i
@@ -120,7 +109,7 @@ class Part1 {
     @Test
     fun task7() {
         val k = (5..10).random()
-        println("Последовательность Фиббоначи из $k чисел")
+        println("Последовательность Фиббоначи из $k чисел:")
         for (i in 0 until k) println(fibonachiMethod(i))
 
     }
@@ -133,7 +122,7 @@ class Part1 {
     @Test
     fun task8() {
         val N = (100..1000).random()
-        println("Сумма цифр натурального числа $N")
+        println("Сумма цифр натурального числа $N:")
         println(sumDigit(N))
     }
 
