ff082af6e2d6b50c7e1a0c4784d06624211a61a6
Yaroslav <rqpr@mail.ru>
2021-10-19 20:24:43 +0300

    Add test

--- src/test/kotlin/tests/Part2.kt
+++ src/test/kotlin/tests/Part2.kt
@@ -1,14 +1,28 @@
 package tests
 
 import org.junit.Test
+import java.io.File
 
 
 class Part2 {
 
     @Test
     fun task1() {
+        val W = "tr"
+        val lineList = mutableListOf<String>()
+        val textList = mutableListOf<String>()
 
-
+        File("part2_task1.txt").useLines { lines -> lines.forEach {
+            lineList.add(it)
+            if (it.indexOf(W)>-1) textList.add(it)
+            }
+        }
+        println("Текст в файле:")
+        lineList.forEach { println(it) }
+        println("Строка(и) имеющая искаемое значение '$W':")
+        textList.forEach { println(it) }
+        }
     }
 
-}
+
+
