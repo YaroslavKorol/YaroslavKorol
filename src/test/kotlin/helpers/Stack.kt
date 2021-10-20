d93e63053c6ac0d2acf951d305894775e78544e5
YaroslavKorol <91884532+YaroslavKorol@users.noreply.github.com>
2021-10-20 09:50:16 +0300

    Update Stack.kt

--- src/test/kotlin/helpers/Stack.kt
+++ src/test/kotlin/helpers/Stack.kt
@@ -3,8 +3,6 @@ package helpers
 class Stack {
     val elements: MutableList<Any> = mutableListOf()
 
-    fun isEmpty() = elements.isEmpty()
-
     fun add(item: Any) {
         elements.add(item)
         println("Добавлен элемент: $item")
@@ -14,7 +12,7 @@ class Stack {
     fun delete(index: Int): Any {
         val item = elements.elementAt(index)
 
-        if (!isEmpty()) {
+        if (!elements.isEmpty()) {
             elements.removeAt(elements.size - (elements.size - index))
             println("Удален элемент: $item")
             println("Элементы стака: $elements")
